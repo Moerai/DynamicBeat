4628ddccd7401daef027986c445d771f8a9388b3
Moerai <skyinbss@gmail.com>
2018-12-16 09:54:24 +0900

    게임 화면을 뿌려줄 Game 클래스 구현해야함

---
+++ src/dynamic_beat/Game.java
@@ -0,0 +1,15 @@
+package dynamic_beat;
+
+import java.awt.Graphics2D;
+
+public class Game extends Thread{
+	public void screenDraw(Graphics2D g) {
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+	
+}
